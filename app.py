c25b43fc1df426cf6f04da7928e0ec470e190b93
lucrecegeraldine-design <lucrecegeraldine@gmail.com>
2026-05-18 20:38:13 +0100

    add test modification for sync check

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 
+st.write("TEST MODIF")
 # =====================================================
 # CONFIGURATION PAGE
 # =====================================================
